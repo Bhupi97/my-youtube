fbfe39aada87e306be83f2f6682621ef576c7721
Bhupinder <Bhupinderpartap97@gmail.com>
2025-01-17 05:54:51 -0500

    Created VideoCard component

--- src/components/VideosContainer.js
+++ src/components/VideosContainer.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideosContainer = () => {
+    const [videos, setVideos] = useState([]);
 
 
     useEffect(() => {
@@ -11,12 +13,13 @@ const VideosContainer = () => {
     const loadVideos = async () => {
         const data = await fetch(YOUTUBE_VIDEOS_API);
         const json = data.json();
-        console.log(json);
+        console.log(json?.items);
+        setVideos(json?.items);
     }
 
     return (
         <div>
-            VideosContainer
+            {videos && <VideoCard info={videos[0]} />}
         </div>
     )
 };
